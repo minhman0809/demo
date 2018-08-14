d597628135e2642c247d456f53c47d5d26eef987
minhman0809 <manminhman0809@gmail.com>
2018-08-14 15:53:42 +0700

    ngay thang nam

---
+++ banggia.php
@@ -0,0 +1,123 @@
+<?php
+session_start();
+include 'config.php';
+include 'head.php';
+?>
+<div class="container">
+    <div class="row">
+        <center><h1><a href="/">Thuê Bot Online tại <?php echo $hometom; ?></a></h1></center>
+        <center><h2><?php echo $title; ?></h2></center><br/>
+
+        <div class="panel panel-primary">
+
+          <div class="panel-heading">Bảng Giá VIP LIKE</div>
+          <div class="panel-body">
+            <li id="trave" class="list-group-item"><center><h3>Bảng Giá CTV</h3> </center></p>
+
+                <div id="my-tab-content" class="tab-content">
+                  <div class="tab-pane active" id="red">
+
+                    <table class="table table-bordered"><b>
+                        <thead>
+                            <tr>
+                                <th>Gói Vip</th>
+                                <th>Số Like</th>
+                                <th>Like/Phút</th>
+                                <th>1 Tháng</th>
+                                <th>2 Tháng</th>
+                                <th>3 Tháng</th>
+                                <th>4 Tháng</th>
+                                <th>6 Tháng</th>
+
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>150</td>
+                                <td>Tùy Chỉnh</td>
+                                <td>50.000 VNĐ</td>
+                                <td>100.000 VNĐ</td>
+                                <td>150.000 VNĐ</td>
+                                <td>200.000 VNĐ</td>
+                                <td>300.000 VNĐ</td>
+
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>300</td>
+                                <td>Tùy Chỉnh</td>
+                                <td>100.000 VNĐ</td>
+                                <td>200.000 VNĐ</td>
+                                <td>300.000 VNĐ</td>
+                                <td>400.000 VNĐ</td>
+                                <td>600.000 VNĐ</td>
+
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>500</td>
+                                <td>Tùy Chỉnh</td>
+                                <td>150.000 VNĐ</td>
+                                <td>300.000 VNĐ</td>
+                                <td>450.000 VNĐ</td>
+                                <td>600.000 VNĐ</td>
+                                <td>900.000 VNĐ</td>
+
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>700</td>
+                                <td>Tùy Chỉnh</td>
+                                <td>200.000 VNĐ</td>
+                                <td>400.000 VNĐ</td>
+                                <td>600.000 VNĐ</td>
+                                <td>800.000 VNĐ</td>
+                                <td>1.200.000 VNĐ</td>
+
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>1000</td>
+                                <td>Tùy Chỉnh</td>
+                                <td>300.000 VNĐ</td>
+                                <td>600.000 VNĐ</td>
+                                <td>900.000 VNĐ</td>
+                                <td>1.200.000 VNĐ</td>
+                                <td>1.800.000 VNĐ</td>
+
+                            </tr>
+                            <tr>
+                               <td>6</td>
+                               <td>1500</td>
+                               <td>Tùy Chỉnh</td>
+                               <td>450.000 VNĐ</td>
+                               <td>900.000 VNĐ</td>
+                               <td>1.350.000 VNĐ</td>
+                               <td>1.800.000 VNĐ</td>
+                               <td>2.700.000 VNĐ</td>
+
+                           </tr>
+
+                           <tr>
+                            <td>7</td>
+                            <td>2000</td>
+                            <td>Tùy Chỉnh</td>
+                            <td>600.000 VNĐ</td>
+                            <td>1.200.000 VNĐ</td>
+                            <td>1.800.000 VNĐ</td>
+                            <td>2.400.000 VNĐ</td>
+                            <td>3.600.000 VNĐ</td>
+
+                        </tr>
+
+
+                    </tbody>
+                </table></b>
+
+
+            </div></div>
+        </div></div>
+    </div>
+</div>
